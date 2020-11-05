6cd6f38c889f7eb5c24c52602110621de0555e4d
Chris <toftsch@gmail.com>
2020-11-05 14:54:48 +1300

    added heroku-postbuild to package

---
+++ build/precache-manifest.b7717248b2942fb6f2ba3853ed3a30f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b82c5847fc11d2c8fd673548d181ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e9d3c407d1640ad702",
+    "url": "/static/css/2.12da0d42.chunk.css"
+  },
+  {
+    "revision": "26f7de587ebd326d6069",
+    "url": "/static/css/main.beeb4e30.chunk.css"
+  },
+  {
+    "revision": "b6e9d3c407d1640ad702",
+    "url": "/static/js/2.a4e2e489.chunk.js"
+  },
+  {
+    "revision": "26f7de587ebd326d6069",
+    "url": "/static/js/main.02130c7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
